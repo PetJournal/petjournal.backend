ba24f1f173495531ba3ddf82b8bf6c4f2195e508
Anderson Naz <anderson_1004@hotmail.com>
2024-05-20 08:02:36 -0300

    feat: create route get for pet

--- src/main/routes/pet-routes.ts
+++ src/main/routes/pet-routes.ts
@@ -1,8 +1,9 @@
 import { type Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makePetRegistryController } from '@/main/factories'
+import { makeLoadPetsController, makePetRegistryController } from '@/main/factories'
 
 export default (router: Router): void => {
+  router.get('/pet', auth, adaptRoute(makeLoadPetsController()))
   router.post('/pet', auth, adaptRoute(makePetRegistryController()))
 }
