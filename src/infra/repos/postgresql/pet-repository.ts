ced7430ede41fefa85265366cda6dffd975aea0e
Rodrigo Meneses <git@rodrigofmeneses.dev>
2024-06-06 19:08:25 -0300

    feat: update pet prisma repository

--- src/infra/repos/postgresql/pet-repository.ts
+++ src/infra/repos/postgresql/pet-repository.ts
@@ -1,7 +1,7 @@
 import { prisma as db } from '@/infra/repos/postgresql/prisma'
-import { type LoadPetByGuardianIdRepository, type AddPetRepository } from '@/data/protocols'
+import { type LoadPetByGuardianIdRepository, type AddPetRepository, type UpdatePetRepository } from '@/data/protocols'
 
-export class PetRepository implements AddPetRepository, LoadPetByGuardianIdRepository {
+export class PetRepository implements AddPetRepository, LoadPetByGuardianIdRepository, UpdatePetRepository {
   async add (params: AddPetRepository.Params): Promise<AddPetRepository.Result> {
     try {
       const pet = await db.pet.create({
@@ -43,6 +43,37 @@ export class PetRepository implements AddPetRepository, LoadPetByGuardianIdRepos
     }
   }
 
+  async update(params: UpdatePetRepository.Params): Promise<UpdatePetRepository.Result> {
+    const { petId, ...updateData } = params
+    const pet = await db.pet.update({
+      data: updateData,
+      where: {
+        id: petId
+      },
+      select: {
+        id: true,
+        specieAlias: true,
+        guardian: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            email: true,
+            phone: true
+          }
+        },
+        specie: true,
+        petName: true,
+        gender: true,
+        breed: true,
+        breedAlias: true,
+        size: true,
+        castrated: true
+      }
+    })
+    return pet
+  }
+    
   async load (guardianId: LoadPetByGuardianIdRepository.Params): Promise<LoadPetByGuardianIdRepository.Result> {
     const pets = await db.pet.findMany({
       where: { guardianId },
