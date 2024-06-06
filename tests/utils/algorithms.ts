7d5034328427df85cd70dd51a106181e95af53cc
Rodrigo Meneses <git@rodrigofmeneses.dev>
2024-06-06 19:09:08 -0300

    test: ensure update pet works propertly

---
+++ tests/utils/algorithms.ts
@@ -0,0 +1,20 @@
+export function getCombinations(obj: any) {
+    const entries = Object.entries(obj);
+    const result: any = [];
+    const f = (prefix: any, entries: any) => {
+        for (let i = 0; i < entries.length; i++) {
+            result.push(Object.fromEntries([...prefix, entries[i]]));
+            f([...prefix, entries[i]], entries.slice(i + 1));
+        }
+    }
+    f([], entries);
+    return result;
+}
+
+const object = {
+    a: 1,
+    b: 2,
+    c: [3, 4]
+};
+
+console.log(getCombinations(object));
