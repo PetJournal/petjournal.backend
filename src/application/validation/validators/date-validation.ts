d1bb7e0424721c6580d4dc303bc2a5d5bca43b9a
matheusgondra <matheusgondra951@gmail.com>
2024-06-07 20:15:20 -0300

    feat: ensure DateValidation call validator with correct value

--- src/application/validation/validators/date-validation.ts
+++ src/application/validation/validators/date-validation.ts
@@ -1,16 +1,21 @@
 import { InvalidParamError } from '@/application/errors'
 import { type Validation } from '@/application/protocols'
+import { type DateValidator } from '../protocols'
 
 export class DateValidation implements Validation {
   private readonly fieldName: string
+  private readonly dateValidator: DateValidator
 
-  constructor (fieldName: string) {
+  constructor (fieldName: string, dateValidator: DateValidator) {
     this.fieldName = fieldName
+    this.dateValidator = dateValidator
   }
 
   validate (input: any): void | Error {
     if (typeof input[this.fieldName] !== 'string') {
       return new InvalidParamError(this.fieldName)
     }
+
+    this.dateValidator.isValid(input[this.fieldName])
   }
 }
