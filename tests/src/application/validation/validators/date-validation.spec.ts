fd0020256c57f33dda375766c55af5c51a816859
matheusgondra <matheusgondra951@gmail.com>
2024-06-07 20:07:29 -0300

    refactor: move sut creation to a factory method

--- tests/src/application/validation/validators/date-validation.spec.ts
+++ tests/src/application/validation/validators/date-validation.spec.ts
@@ -1,9 +1,20 @@
 import { InvalidParamError } from '@/application/errors'
 import { DateValidation } from '@/application/validation'
 
+interface SutTypes {
+  sut: DateValidation
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new DateValidation('date')
+  return {
+    sut
+  }
+}
+
 describe('DateValidation', () => {
   it('Should return an InvalidParamError if date is not a string', () => {
-    const sut = new DateValidation('date')
+    const { sut } = makeSut()
     const error = sut.validate({ date: 1 })
     expect(error).toEqual(new InvalidParamError('date'))
   })
