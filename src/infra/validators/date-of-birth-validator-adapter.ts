889fd554022dd45e0c08dfaf089c339b266b6f91
matheusgondra <matheusgondra951@gmail.com>
2024-06-07 21:10:28 -0300

    feat: ensure DateOfBirthValidatorAdapter return false if date is a future date

--- src/infra/validators/date-of-birth-validator-adapter.ts
+++ src/infra/validators/date-of-birth-validator-adapter.ts
@@ -2,9 +2,15 @@ import { type DateValidator } from '@/application/validation'
 import validator from 'validator'
 export class DateOfBirthValidatorAdapter implements DateValidator {
   isValid (date: string): boolean {
-    if (isNaN(new Date(date).getTime())) {
+    const dateOfValidation = new Date(date)
+    if (isNaN(dateOfValidation.getTime())) {
       return false
     }
+
+    if (dateOfValidation > new Date()) {
+      return false
+    }
+
     const isValid = validator.isISO8601(date)
     return isValid
   }
