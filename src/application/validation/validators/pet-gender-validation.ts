cb5f4991e0156a8436265cb345a9adb18f78230c
Anderson Naz <anderson_1004@hotmail.com>
2024-06-27 17:48:36 -0300

    refactor: change class PetGenderValidation

--- src/application/validation/validators/pet-gender-validation.ts
+++ src/application/validation/validators/pet-gender-validation.ts
@@ -1,19 +1,24 @@
 import { InvalidParamError } from '@/application/errors'
-import { PetGender } from '@/domain/models/pet'
+import { type Validation } from '@/application/protocols'
+import { type PetGenderValidator } from '../protocols'
 
-export class PetGenderValidation {
-  constructor (
-    private readonly fieldName: string
-  ) {}
+export class PetGenderValidation implements Validation {
+  private readonly fieldName: string
+  private readonly validator: PetGenderValidator
+
+  constructor (fieldName: string, validator: PetGenderValidator) {
+    this.fieldName = fieldName
+    this.validator = validator
+  }
 
   validate (input: any): Error | void {
     const gender = input[this.fieldName]
 
-    if (typeof gender !== 'string') {
+    if (typeof gender !== 'string' && input[this.fieldName] !== null) {
       return new InvalidParamError(this.fieldName)
     }
-
-    if (!Object.values(PetGender).includes(input[this.fieldName])) {
+    const isValid = this.validator.isValid(input, this.fieldName)
+    if (!isValid) {
       return new InvalidParamError(this.fieldName)
     }
   }
