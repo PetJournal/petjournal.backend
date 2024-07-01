732c7baf843e447737e2eab39fdf514e66e68d65
Anderson Naz <anderson_1004@hotmail.com>
2024-07-01 13:26:05 -0300

    feat: create delete pet route

--- src/main/routes/pet-routes.ts
+++ src/main/routes/pet-routes.ts
@@ -1,10 +1,11 @@
 import { type Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
 import { auth } from '@/main/middlewares'
-import { makeLoadPetsController, makePetRegistryController, makeUpdatePetController } from '@/main/factories'
+import { makeLoadPetsController, makePetDeleteController, makePetRegistryController, makeUpdatePetController } from '@/main/factories'
 
 export default (router: Router): void => {
   router.get('/pet', auth, adaptRoute(makeLoadPetsController()))
   router.post('/pet', auth, adaptRoute(makePetRegistryController()))
   router.put('/pet/:petId', auth, adaptRoute(makeUpdatePetController()))
+  router.delete('/pet/:petId', auth, adaptRoute(makePetDeleteController()))
 }
