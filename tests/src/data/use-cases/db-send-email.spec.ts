474725ed765855c73d3d4756b391321b4b1b7c0b
matheusgondra <matheusgondra951@gmail.com>
2024-08-13 21:45:51 -0300

    refactor: move sut creation to a factory method

--- tests/src/data/use-cases/db-send-email.spec.ts
+++ tests/src/data/use-cases/db-send-email.spec.ts
@@ -1,20 +1,35 @@
+import { type LoadGuardianByEmailRepository } from '@/data/protocols'
 import { DbSendEmail } from '@/data/use-cases'
 import { makeFakeGuardianRepository } from '@/tests/utils'
 
+interface SutTypes {
+  sut: DbSendEmail
+  guardianRepositoryStub: LoadGuardianByEmailRepository
+}
+
+const makeSut = (): SutTypes => {
+  const guardianRepositoryStub = makeFakeGuardianRepository()
+  const sut = new DbSendEmail({ guardianService: guardianRepositoryStub })
+  return {
+    sut,
+    guardianRepositoryStub
+  }
+}
+
 describe('DbSendEmail', () => {
+  const data = { email: 'any_email' }
+
   it('Should call GuardianRepository with correct value', async () => {
-    const guardianRepositoryStub = makeFakeGuardianRepository()
-    const sut = new DbSendEmail({ guardianService: guardianRepositoryStub })
+    const { sut, guardianRepositoryStub } = makeSut()
     const loadByEmailSpy = jest.spyOn(guardianRepositoryStub, 'loadByEmail')
-    await sut.send({ email: 'any_email' })
+    await sut.send(data)
     expect(loadByEmailSpy).toHaveBeenCalledWith('any_email')
   })
 
   it('Should throw if GuardianRepository throws', async () => {
-    const guardianRepositoryStub = makeFakeGuardianRepository()
-    const sut = new DbSendEmail({ guardianService: guardianRepositoryStub })
+    const { sut, guardianRepositoryStub } = makeSut()
     jest.spyOn(guardianRepositoryStub, 'loadByEmail').mockRejectedValueOnce(new Error())
-    const promise = sut.send({ email: 'any_email' })
+    const promise = sut.send(data)
     await expect(promise).rejects.toThrow()
   })
 })
