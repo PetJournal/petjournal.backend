4f9428e08218c31de43b833b61221fa30cb84f65
matheusgondra <matheusgondra951@gmail.com>
2024-09-02 20:02:58 -0300

    test: ensure AccountConfirmationMiddleware returns 500 if loadByEmail throws

--- tests/src/application/middlewares/account-confirmation.spec.ts
+++ tests/src/application/middlewares/account-confirmation.spec.ts
@@ -1,3 +1,4 @@
+import { serverError } from '@/application/helpers'
 import { AccountConfirmationMiddleware } from '@/application/middlewares/account-confirmation'
 import { type LoadGuardianByEmailRepository } from '@/data/protocols'
 import { makeFakeGuardianRepository } from '@/tests/utils'
@@ -18,17 +19,25 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AccountConfirmationMiddleware', () => {
+  const httpRequest = {
+    body: {
+      email: 'any_wmail@mail.com'
+    }
+  }
+
   describe('GuardianRepository', () => {
     it('Should call loadByEmail with correct value', async () => {
       const { sut, guardianRepositoryStub } = makeSut()
       const loadByEmailSpy = jest.spyOn(guardianRepositoryStub, 'loadByEmail')
-      const httpRequest = {
-        body: {
-          email: 'any_wmail@mail.com'
-        }
-      }
       await sut.handle(httpRequest)
       expect(loadByEmailSpy).toBeCalledWith(httpRequest.body.email)
+    })
+
+    it('Shoul return 500 (ServerError) if loadByEmail method throws', async () => {
+      const { sut, guardianRepositoryStub } = makeSut()
+      jest.spyOn(guardianRepositoryStub, 'loadByEmail').mockRejectedValueOnce(new Error())
+      const httpResponse = await sut.handle(httpRequest)
+      expect(httpResponse).toEqual(serverError(new Error()))
     })
   })
 })
